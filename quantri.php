d9542f8884fbbea9ec7440601252882ed5648da0
judoittt <your_email@example.com>
2025-04-11 23:06:41 +0700

    change 2

--- quantri.php
+++ quantri.php
@@ -1,4 +1,9 @@
 <?php 
+    session_start();
+    if(isset($_SESSION['admin'])){
+    header( 'Location: index.php?act=trangchu');
+
+    }
      if(!isset($_SESSION['username']) || !isset($_SESSION['password']) || !isset($_SESSION['admin'])){
         echo'<div style="margin: 0 auto; width: 100%; max-width: 600px;">';
         include './view/dangnhap.php';
@@ -9,4 +14,7 @@
       
 
     }
+  
+ 
+
 ?>
